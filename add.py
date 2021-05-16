b62cee9948a89433b00c78f14e3d860975ccbb65
PavanSuhas3457 <69577841+PavanSuhas3457@users.noreply.github.com>
2021-05-16 16:04:49 +0530

    Update add.py

--- add.py
+++ add.py
@@ -11,3 +11,13 @@ print('Difference of the two arguments is',(num1-num2))
 print('Product of the two arguments is',(num1*num2))
 print('Division of the two arguments is',(num1//num2))
 print('Modulus of the two arguments is',(num1%num2))
+
+'''
+Output:
+C:\Users\Lenovo\OneDrive\Documents\Python Lab Programs>python add.py 10 5
+Sum of the two arguments is 15
+Difference of the two arguments is 5
+Product of the two arguments is 50
+Division of the two arguments is 2
+Modulus of the two arguments is 0
+'''
