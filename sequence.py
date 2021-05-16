e75c753380eaeff012f0a614b0b121ca2913326e
PavanSuhas3457 <69577841+PavanSuhas3457@users.noreply.github.com>
2021-05-16 16:11:15 +0530

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -10,3 +10,8 @@ for i in range (1,num+1):
             count += 1
     if count == 2:
         print(i,end = ' ')
+'''
+Output:
+Enter Your Range:100
+2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+'''
