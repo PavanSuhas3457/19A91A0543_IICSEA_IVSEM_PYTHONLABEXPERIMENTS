a58aae77772296ef0987566533f9d8c600275a14
PavanSuhas3457 <69577841+PavanSuhas3457@users.noreply.github.com>
2021-05-16 16:10:17 +0530

    Update pythogoras.py

--- pythogoras.py
+++ pythogoras.py
@@ -9,3 +9,11 @@ y1 = int(input('Enter y1:'))
 y2 = int(input('Enter y2:'))
 res = (m.sqrt((x2-x1)**2+(y2-y1)**2))
 print('The distance between',(x1,y1),'and',(x2,y2),'is',round(res,2))
+'''
+Output:
+Enter x1:4
+Enter x2:8
+Enter y1:9
+Enter y2:6
+The distance between (4, 9) and (8, 6) is 5.0
+'''
