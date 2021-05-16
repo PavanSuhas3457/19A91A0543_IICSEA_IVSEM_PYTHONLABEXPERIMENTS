f795c4e8962a9907f4a4d3bb5abb79259ebefbb0
PavanSuhas3457 <69577841+PavanSuhas3457@users.noreply.github.com>
2021-05-16 16:22:11 +0530

    Update Indentation.py

--- Indentation.py
+++ Indentation.py
@@ -1,4 +1,29 @@
+'''
+1.2) Implement a Python Script to purposefully
+     raise Indentation Error and Correct it
+'''
 num = int(input('Enter a Number:'))
+i = 1
 while i<num:
 print(i)    #Raises an Indentation Error
-    i+=1
+    i += 1
+
+num = int(input('Enter a Number:'))
+i = 1
+while i<num:
+    print(i)    #Correction of this Indentation Error
+    i += 1
+'''
+Output:
+Enter a Number:10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+'''
